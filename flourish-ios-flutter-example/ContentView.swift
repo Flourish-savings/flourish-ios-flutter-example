6309b31b8fecad81f00b27be077bc0cd6464d061
Rafael Kerr <rafael.kerr@gmail.com>
2024-01-10 16:59:27 -0300

    Initial Commit

--- flourish-ios-flutter-example/ContentView.swift
+++ flourish-ios-flutter-example/ContentView.swift
@@ -1,24 +1,47 @@
-//
-//  ContentView.swift
-//  flourish-ios-flutter-example
-//
-//  Created by Rafael Wilber Kerr on 10/01/24.
-//
-
 import SwiftUI
+import Flutter
 
 struct ContentView: View {
+    @EnvironmentObject var flutterDependencies: FlutterDependencies
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            VStack(spacing: 10) {
+                Image(.logo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(EdgeInsets(top: 30, leading: 100, bottom: 100, trailing: 100))
+                Text("This is a native iOS App, click the button below to open our \n module in Flutter")
+                    .multilineTextAlignment(.center)
+                    .padding(EdgeInsets(top: 10, leading: 30, bottom: 100, trailing: 30))
+                
+                Button("Open FlourishFI Flutter module") {
+                    showFlutter()
+                }
+                .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func showFlutter() {
+        guard
+          let windowScene = UIApplication.shared.connectedScenes
+            .first(where: { $0.activationState == .foregroundActive && $0 is UIWindowScene }) as? UIWindowScene,
+          let window = windowScene.windows.first(where: \.isKeyWindow),
+          let rootViewController = window.rootViewController
+        else { return }
+
+        // Create the FlutterViewController.
+        let flutterViewController = FlutterViewController(
+          engine: flutterDependencies.flutterEngine,
+          nibName: nil,
+          bundle: nil)
+        flutterViewController.modalPresentationStyle = .overCurrentContext
+        flutterViewController.isViewOpaque = false
+
+        rootViewController.present(flutterViewController, animated: true)
     }
 }
 
-#Preview {
-    ContentView()
-}
--- flourish-ios-flutter-example/flourish_ios_flutter_exampleApp.swift
+++ flourish-ios-flutter-example/flourish_ios_flutter_exampleApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Flutter
+  // The following library connects plugins with iOS platform code to this app.
+import FlutterPluginRegistrant
+
+class FlutterDependencies: ObservableObject {
+  let flutterEngine = FlutterEngine(name: "flourish")
+  init(){
+    // Runs the default Dart entrypoint with a default Flutter route.
+    flutterEngine.run()
+    // Connects plugins with iOS platform code to this app.
+    GeneratedPluginRegistrant.register(with: self.flutterEngine);
+  }
+}
 
 @main
 struct flourish_ios_flutter_exampleApp: App {
+    @StateObject var flutterDependencies = FlutterDependencies()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(flutterDependencies)
         }
     }
 }
