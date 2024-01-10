24a17e99c47c23c48b432e39329bf3aa512c32d6
Rafael Kerr <rafael.kerr@gmail.com>
2024-01-10 10:31:11 -0300

    Initial Commit

---
+++ flourish-ios-flutter-example/flourish_ios_flutter_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  flourish_ios_flutter_exampleApp.swift
+//  flourish-ios-flutter-example
+//
+//  Created by Rafael Wilber Kerr on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct flourish_ios_flutter_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
